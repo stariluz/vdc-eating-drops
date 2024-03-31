9920a3defcef9c6558e5e16df878c443d33a0a0e
Adrian González <adrigondo.profesional@gmail.com>
2024-03-31 16:47:11 -0600

    Add: movement and animations
    The animations were separeated in layers for mesh gesture animations with movement animations.

--- Assets/GameElements/Characters/PlayerMovement.cs
+++ Assets/GameElements/Characters/PlayerMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    new Rigidbody2D rigidbody2D;
+    Animator animator;
+    public bool isMoving=false;
+    public float speed=2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D=GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float deltaX=Input.GetAxis("Horizontal");
+        if(deltaX==0){
+            isMoving=false;
+            StopMoving();
+        }else{
+            if(deltaX>0){
+                RightMovement(deltaX);
+            }else if(deltaX<0){
+                LeftMovement(deltaX);
+            }
+            isMoving=true;
+        }
+    }
+    void LeftMovement(float deltaX){
+        animator.SetBool("IsMovingLeft", true);
+        this.rigidbody2D.velocity=new Vector2(deltaX*speed,0);
+    }
+    void RightMovement(float deltaX){
+        animator.SetBool("IsMovingRight", true);
+        this.rigidbody2D.velocity=new Vector2(deltaX*speed,0);
+    }
+    void StopMoving(){
+        animator.SetBool("IsMovingLeft", false);
+        animator.SetBool("IsMovingRight", false);
+        this.rigidbody2D.velocity=new Vector2(0,0);
     }
 }
